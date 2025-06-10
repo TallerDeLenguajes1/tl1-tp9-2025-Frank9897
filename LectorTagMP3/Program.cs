edc8ab18599cb3e995edb427c59178277b7c9a55
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-10 18:30:15 -0300

    Verificacion de existencia de direccion

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string directorio = "";
+
+while (true)
+{
+    Console.WriteLine("Ingrese la direccion del archivo .mp3 que desea analizar");
+    directorio = Console.ReadLine();
+    if (Directory.Exists(directorio))
+    {
+        break;
+    }else
+    {
+        Console.WriteLine("Directorio no encontrado, intente de nuevo...");
+    }
+}
+
