1c2cca4908c4d635d9265d46944f063b83cb4f53
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-10 16:20:22 -0300

    Comienzo el Ejercicio 1 del TP9

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+string directorio = "";
+
+while (true)
+{
+    Console.WriteLine("Ingrese un PATH del directorio que desea analizar");
+    directorio = Console.ReadLine();
+    if (Directory.Exists(directorio))
+    {
+        break;
+    }else
+    {
+        Console.WriteLine("No se encontro el directorio, Intente de nuevo...");
+    }
+}
